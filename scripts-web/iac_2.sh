d3d23b0d397bfa6433192909177b1250ec8a04b3
Bu9iu <root@192.168.1.13>
2022-11-11 17:04:56 -0300

    Apache v1.0

---
+++ scripts-web/iac_2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Atualizando o Servidor..."
+yum update
+yum upgrade -y
+dnf install httpd -y
+systemctl enable --now httpd.service
+
+echo "Baixando a aplicação..."
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio-main
+cp -R * /var/www/html
+
